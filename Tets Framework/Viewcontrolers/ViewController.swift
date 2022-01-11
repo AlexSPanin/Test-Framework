95bf5269149af68ca11891432d77cd4c7ba2416e
Panin <panin1970@gmail.com>
2022-01-11 18:12:07 +0300

    add func view label

--- Tets Framework/Viewcontrolers/ViewController.swift
+++ Tets Framework/Viewcontrolers/ViewController.swift
@@ -9,11 +9,62 @@ import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelAnimationSpringView: SpringView!
+    @IBOutlet weak var labelSpringButton: SpringButton!
+   
+    @IBOutlet var pressetLabel: [UILabel]!
+    
+    
+    
+    private var pressed = ""
+    private var curve = ""
+    private var force: CGFloat = 0
+    private var delay: CGFloat = 0
+    private var duration: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewPresset()
+        
     }
+    
 
+    @IBAction func runAnimationSpringButton(_ sender: SpringButton) {
+        
+        setRandomPressetSpring()
+        
+//        for count in Spring.AnimationPreset.allCases {
 
+        labelAnimationSpringView.animation = Spring.AnimationPreset.fadeInLeft.rawValue
+        labelSpringButton.setTitle("RUN \(pressed)", for: .normal)
+
+            labelAnimationSpringView.curve = Spring.AnimationCurve.easeOut.rawValue
+        labelAnimationSpringView.force = force
+        labelAnimationSpringView.delay = delay
+        labelAnimationSpringView.duration = duration
+            labelAnimationSpringView.animate()
+            
+ //       }
+    }
 }
+
+extension ViewController {
+    
+    private func setRandomPressetSpring() {
+        force = CGFloat.random(in: 0...2)
+        delay = CGFloat.random(in: 0...1)
+        duration = CGFloat.random(in: 0...3)
+        
+        
+    }
+    
+    private func viewPresset() {
+        pressetLabel[0].text = labelAnimationSpringView.animation
+        pressetLabel[1].text = labelAnimationSpringView.curve
+        pressetLabel[2].text = String(Float(force))
+        pressetLabel[3].text = String(Float(delay))
+        pressetLabel[4].text = String(Float(duration))
+    }
+}
+
 
